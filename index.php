379a433df0ca19beaa475ef04aea1ad39c7d00b3
Jirayu Limjinda <jir4yu.l@gmail.com>
2014-09-29 22:48:53 +0700

    add environment

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Jir4yu.me</title>
+	<meta name="keywords" content="Jir4yu.me, JindaTheme, WordPress, Responsive Theme" />
+	<meta name="description" content="Jir4yu.me ความรู้สึกที่ตอนไหนๆก็(ไม่)เหมือนกัน" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/bower_components/foundation/css/normalize.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/bower_components/foundation/css/foundation.css">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_uri() ?>">
+	<script src="<?php echo get_stylesheet_directory_uri() ?>/bower_components/modernizr/modernizr.js"></script>
+</head>
+<body>
+	<h1>hello</h1>
+
+
+	<!-- livereload -->
+	<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+	<!-- livereload -->
+	<script src="<?php echo get_stylesheet_directory_uri() ?>/bower_components/jquery/dist/jquery.min.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri() ?>/bower_components/fastclick/lib/fastclick.js"></script>
+	<script src="<?php echo get_stylesheet_directory_uri() ?>/bower_components/foundation/js/foundation.min.js"></script>
+</body>
+</html>
