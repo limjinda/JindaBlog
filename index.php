c26d21f78368a9e07735acc25ecd65650e715cc8
Jirayu Limjinda <jir4yu.l@gmail.com>
2014-09-30 08:44:48 +0700

    add main HTML

--- index.php
+++ index.php
@@ -16,7 +16,66 @@
 	<script src="<?php echo get_stylesheet_directory_uri() ?>/bower_components/modernizr/modernizr.js"></script>
 </head>
 <body>
-	<h1>hello</h1>
+
+	<div id="wrapper">
+
+		<header class="main-header-block">
+			<div class="hero-unit">
+				<h1 class="site-title">Jir4yu.me</h1>
+				<h2 class="site-slogan">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h2>
+			</div>
+		</header>
+
+		<main>
+			<div class="hero-unit">
+				<div class="main-sidebar-block">
+					<div class="writer-widget widget">
+						<img src="http://www.placehold.it/120x120" class="writer-avatar" alt="Avatar" />
+						<p class="writer-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro cumque qui, architecto eum natus voluptatem aut sint dolore dignissimos delectus aliquam laboriosam rerum nemo sunt omnis nihil vitae cupiditate in!</p>
+					</div>
+					<div class="nav-widget widget">
+						<nav class="main-navigation">
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About</a></li>
+								<li><a href="#">Categories</a></li>
+								<li><a href="#">JindaTheme</a></li>
+							</ul>
+						</nav>
+					</div>
+				</div>
+				<div class="main-content-block"></div>
+			</div>
+		</main>
+
+		<footer class="footer-block">
+			<div class="hero-unit">
+				<p class="footer-copyright">
+					all posts in <strong>Jir4yu.me</strong> was written by <a href="https://plus.google.com/114178088498011147130?rel=author" target="_blank">Jirayu L</a> under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a>
+				</p>
+				<p class="footer-credit">
+					<a href="https://github.com/jir4yu/JindaBlog" target="_blank" title="JiraBlog">JiraBlog</a> is an opensource WordPress theme, created by <a href="http://www.jindatheme.com" target="_blank" title="JindaTheme">JindaTheme</a>
+				</p>
+				<ul class="footer-social-block">
+					<li>
+						<a href="http://www.facebook.com/byjir4yu" title="My Facebook" target="_blank">
+							<i class="fa fa-facebook-square facebook"></i>
+						</a>
+					</li>
+					<li>
+						<a href="http://www.twitter.com/jir4yu" title="My Twitter" target="_blank">
+							<i class="fa fa-twitter-square twitter"></i>
+						</a>
+					</li>
+					<li>
+						<a href="https://plus.google.com/+JirayuL" title="My Google Plus" target="_blank">
+							<i class="fa fa-google-plus-square gplus"></i>
+						</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	</div>
 
 
 	<!-- livereload -->
