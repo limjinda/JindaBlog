464cf059de3011374fe78d2c2b05dea63c634b62
Jirayu Limjinda <jir4yu.l@gmail.com>
2014-10-17 14:05:42 +0700

    add responsive home layout

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 			<div class="hero-unit">
 				<div class="main-author">
 					<img src="http://www.placehold.it/120x120" class="writer-avatar uk-border-circle" alt="Avatar" />
-					<h1 class="site-title">Jir4yu.me</h1>
+					<h1 class="site-title"><a href="<? home_url() ?>">Jir4yu.me</a></h1>
 					<h2 class="site-slogan">Lorem ipsum dolor sit amet, consectetur</h2>
 				</div>
 				<div class="short-divider"></div>
@@ -43,58 +43,57 @@
 		<main class="home-right-block">
 			<div class="hero-unit">
 				<!-- loop -->
-				<ul class="home-post-list uk-list">
-					<li>
-						<div class="uk-grid">
-							<div class="uk-width-1-1">
-								<article class="uk-article">
-									<img src="http://www.placehold.it/768x294" class="home-post-image" alt="" />
-									<h3 class="uk-article-title post-title">Lorem Ipsum</h3>
-									<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
-									<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
-									<hr class="uk-article-divider">
-								</article>
-							</div>
+				<div class="home-post-list">
+					<div class="uk-grid uk-grid-small data-uk-grid-match">
+						<div class="uk-width-1-1 uk-width-small-1-2 uk-width-medium-1-1 post-block">
+							<article class="uk-article">
+								<img src="http://www.placehold.it/768x475" class="home-post-image" alt="" />
+								<h3 class="uk-article-title post-title"><a href="#">Lorem Ipsum</a></h3>
+								<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
+								<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
+								<hr class="uk-article-divider">
+							</article>
 						</div>
-					</li>
-					<li>
-						<div class="uk-grid">
-							<div class="uk-width-1-1">
-								<article class="uk-article">
-									<img src="http://www.placehold.it/768x294" class="home-post-image" alt="" />
-									<h3 class="uk-article-title post-title">Lorem Ipsum</h3>
-									<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
-									<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
-									<hr class="uk-article-divider">
-								</article>
-							</div>
+						<div class="uk-width-1-1 uk-width-small-1-2 uk-width-medium-1-1 post-block">
+							<article class="uk-article">
+								<img src="http://www.placehold.it/768x475" class="home-post-image" alt="" />
+								<h3 class="uk-article-title post-title"><a href="#">Lorem Ipsum</a></h3>
+								<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
+								<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
+								<hr class="uk-article-divider">
+							</article>
 						</div>
-					</li>
-					<li>
-						<div class="uk-grid">
-							<div class="uk-width-1-1">
-								<article class="uk-article">
-									<img src="http://www.placehold.it/768x294" class="home-post-image" alt="" />
-									<h3 class="uk-article-title post-title">Lorem Ipsum</h3>
-									<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
-									<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
-									<hr class="uk-article-divider">
-								</article>
-							</div>
+						<div class="uk-width-1-1 uk-width-small-1-2 uk-width-medium-1-1 post-block">
+							<article class="uk-article">
+								<img src="http://www.placehold.it/768x475" class="home-post-image" alt="" />
+								<h3 class="uk-article-title post-title"><a href="#">Lorem Ipsum</a></h3>
+								<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
+								<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
+								<hr class="uk-article-divider">
+							</article>
 						</div>
-					</li>
-
-				</ul>
+						<div class="uk-width-1-1 uk-width-small-1-2 uk-width-medium-1-1 post-block">
+							<article class="uk-article">
+								<img src="http://www.placehold.it/768x475" class="home-post-image" alt="" />
+								<h3 class="uk-article-title post-title"><a href="#">Lorem Ipsum</a></h3>
+								<p class="uk-article-meta post-meta">on 19 Feb 2015, Posted in Blog</p>
+								<p class="post-excerpt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit accusamus quidem optio, aspernatur esse! Eveniet et eius cumque optio libero, ex fugiat, officiis accusantium consectetur vitae esse necessitatibus illum quo! <a href="#" class="more-text">[read more]</a></p>
+								<hr class="uk-article-divider">
+							</article>
+						</div>
+					</div>
+				</div>
 				<!-- /loop -->
+
 				<footer class="footer-block">
 					<div class="hero-unit">
 						<p class="footer-copyright uk-text-center">
-							all posts in <strong>Jir4yu.me</strong> was written by <a href="https://plus.google.com/114178088498011147130?rel=author" target="_blank">Jirayu L</a> under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a>
+							all posts in <a href="<?= home_url() ?>">Jir4yu.me</a> was written by <a href="https://plus.google.com/114178088498011147130?rel=author" target="_blank">Jirayu L</a> under <a href="http://creativecommons.org/licenses/by-nc-nd/4.0/" target="_blank">CC BY-NC-ND 4.0</a>
 						</p>
 						<p class="footer-credit uk-text-center">
-							<a href="https://github.com/jir4yu/JindaBlog" target="_blank" title="JiraBlog">JiraBlog</a> is an opensource WordPress theme, created by <a href="http://www.jindatheme.com" target="_blank" title="JindaTheme">JindaTheme</a>
+							<a href="https://github.com/jir4yu/JindaBlog" target="_blank" title="JiraBlog">JindaBlog</a> is an opensource WordPress theme, created by <a href="http://www.jindatheme.com" target="_blank" title="JindaTheme">JindaTheme</a>
 						</p>
-						<ul class="footer-social-block">
+						<ul class="footer-social-list">
 							<li>
 								<a href="http://www.facebook.com/byjir4yu" title="My Facebook" target="_blank">
 									<i class="uk-icon-facebook-square facebook"></i>
@@ -125,6 +124,8 @@
 				</footer>
 			</div>
 		</main>
+
+		<div class="clearfix"></div>
 
 	</div>
 
